8d63fd673388111b01522fff71debc9b460797c8
Bill Drosatos <b.drosat@hotmail.com>
2019-02-27 23:05:51 +0100

    test

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 import file_parser
 
-lines = file_parser.read_file("input/easy.input")
+file_names = [
+    "input/easy.input",
+    "input/medium.input"
+]
+lines_per_file = {}
+for file_name in file_names:
+    lines_per_file[file_name] = file_parser.read_file(file_name)
+
+for file_name, lines in lines_per_file.items():
+    f = file_parser.write_file(file_name.replace("input", "output"))
+    f.write('\n'.join(map(str, lines)))
+    f.write("\nOUTPUT")
+
